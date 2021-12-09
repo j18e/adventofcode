fbc494698cad20cdf1ca9321627428c4a102d9f8
Jamie Wiebe <jamiewiebe@gmail.com>
2021-12-09 13:03:13 +0100

    complete day 6

--- 2021/day6/main.go
+++ 2021/day6/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strings"
-	"sync"
 
 	"github.com/j18e/adventofcode/pkg/converting"
 	"github.com/j18e/adventofcode/pkg/inputting"
@@ -17,7 +16,7 @@ const (
 func main() {
 	input := parseInput(inputting.GetInput("input.txt"))
 	fmt.Println(part1(input))
-	// fmt.Println(part2(input))
+	fmt.Println(part2(input))
 }
 
 func part1(fish []int) int {
@@ -36,37 +35,32 @@ func parseInput(input string) []int {
 	return res
 }
 
-func countLists(lists [][]int) int {
+func countFish(fish map[int]int) int {
 	res := 0
-	for _, l := range lists {
-		res += len(l)
+	for _, cnt := range fish {
+		res += cnt
 	}
 	return res
 }
 
-func reproduce(fish []int, days int) int {
-	var schools [][]int
-	for _, f := range fish {
-		schools = append(schools, []int{f})
+func reproduce(startingFish []int, days int) int {
+	fish := make(map[int]int, 9)
+	for _, f := range startingFish {
+		fish[f]++
 	}
+
 	for d := 0; d < days; d++ {
-		fmt.Println(d, len(schools), len(schools[0]), countLists(schools))
-		var wg sync.WaitGroup
-		for si := range schools {
-			si := si
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for i := range schools[si] {
-					schools[si][i]--
-					if schools[si][i] == -1 {
-						schools[si] = append(schools[si], newAge)
-						schools[si][i] = age
-					}
-				}
-			}()
+		fish = map[int]int{
+			0: fish[1],
+			1: fish[2],
+			2: fish[3],
+			3: fish[4],
+			4: fish[5],
+			5: fish[6],
+			6: fish[7] + fish[0],
+			7: fish[8],
+			8: fish[0],
 		}
-		wg.Wait()
 	}
-	return countLists(schools)
+	return countFish(fish)
 }
